73553c989a54ba1a082d77cc52bd92a7f85f5fcc
Tiago Aparício <tiago.aparicio@innowave.tech>
2018-10-02 15:23:39 +0100

    TodoAppStep3-addDebug

--- src/app/todo-app/todo-list/todo-list.component.js
+++ src/app/todo-app/todo-list/todo-list.component.js
@@ -4,23 +4,28 @@ const TodoList = ({ todoList, toggleTodo }) => {
 
     const handleToDoCheckBoxChange = (event, i) => {
         const value = event.target.checked;
-        toggleTodo({index: i, value});
+        toggleTodo({ index: i, value });
     }
 
     return (
-        <ul>
-            {todoList.map((obj, i) => (
-                <li key={i}>
-                    <input
-                        name={i}
-                        type="checkbox"
-                        checked={obj.done}
-                        onChange={ e => handleToDoCheckBoxChange(e, i) }
-                    />
-                    {obj.name}
-                </li>)
-            )}
-        </ul>
+        <div>
+            <ul>
+                {todoList.map((obj, i) => (
+                    <li key={i}>
+                        <input
+                            name={i}
+                            type="checkbox"
+                            checked={obj.done}
+                            onChange={e => handleToDoCheckBoxChange(e, i)}
+                        />
+                        {obj.name}
+                    </li>)
+                )}
+            </ul>
+            <hr />
+            <h2>Debug Stuff</h2>
+            <pre>{JSON.stringify(todoList, null, 2)}</pre>
+        </div>
     )
 };
 
